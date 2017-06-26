74cacb6a5da11a1c0cda708dfbb8958cfd1a3d45
dhavalrajani <dhaval.rajani@raksanconsutlting.com>
2017-06-26 15:55:03 +0530

    initial commit

---
+++ describe-example.js
@@ -0,0 +1,70 @@
+import assert from 'assert';
+import { test, testSuite, setup, teardown,testDone } from './describe';
+
+const obj = {};
+testSuite('True Or False? ', () => {
+  testSuite('setup', () => {
+    test('should setup num', () => {
+      assert.equal(obj.num, 2);
+    });
+    setup(() => {
+      obj.num = 2;
+    });
+    teardown(() => {
+      obj.num = null;
+    });
+  });
+
+  testSuite('teardown', () => {
+    test('should teardown num', () => {
+      assert.equal(obj.num, null);
+    });
+  });
+
+  testSuite('truthy => ', () => {
+    test('empty array', () => {
+      assert.equal(!![0], true);
+    });
+
+    test('empty object', () => {
+      assert.equal(!!{}, true);
+    });
+  });
+
+  testSuite('falsy => ', () => {
+    testSuite('undefined & nil', () => {
+      test('undefined', () => {
+        assert.equal(!(void 0), true);
+      });
+      test('null', () => {
+        assert.equal(!null, true);
+      });
+    });
+
+    test('should test ![] === true ', (callDone) => {
+
+
+        var timeOutVar = setTimeout(() => {
+            try {
+                assert.equal(![], true);
+                callDone(null); // success case
+            } catch (err) {
+                callDone(err); // error case
+            }
+        });
+
+
+        function callDone(err){
+          testDone(err,global, () => {
+          });
+        }
+    });
+
+    test('!NaN === true', () => {
+      assert.equal(!NaN, true);
+    });
+    test('!(empty string) === true', () => {
+      assert.equal(!'', true);
+    });
+  });
+});
---
+++ describe.js
@@ -0,0 +1,69 @@
+const success = desc => console.log(`${desc} : Pass`);
+const failure = (desc, msg) => console.log(`:( ${desc} : Fail => ${msg}`);
+const log = desc => console.log(desc);
+
+const activity = {};
+const stack = [];
+const isEmptyStack = () => stack.length === 0;
+const stackTop = () => stack[stack.length - 1];
+const ctx = {};
+
+const spush = (key, val) => stackTop()[key].push(val);
+const start = new Date();
+var finished;
+const indentedTitle = ctxt =>
+  `${stack.map(() => '   ').join('')}${ctxt}`;
+
+const newTop = title =>
+  ({ title, tests: [], setup: [], teardown: [], testSuites: [] });
+
+const execTop = () => 'setup tests testSuites teardown'.split(' ')
+  .forEach(key => stackTop()[key].forEach(activity[key]));
+
+const execTestSuite = (title, testSuiteFn) => {
+  log(indentedTitle(title));
+  stack.push(newTop(title));
+  testSuiteFn.call(ctx);   // collect testSuites, setup, teardown and it.
+  execTop();               // execute them
+  stack.pop();
+};
+
+const reportTests = (fn, title) => {
+  const desc = indentedTitle(title);
+
+  try {
+    fn.call(ctx);
+    success(desc);
+  } catch (e) {
+    failure(desc, e.message);
+  }
+};
+
+activity.setup = fn => fn.call(ctx);
+activity.teardown = fn => fn.call(ctx);
+activity.testSuites = ([title, testFn]) =>
+  execTestSuite(title, testFn);
+activity.tests = ([title, testFn]) =>
+  reportTests(testFn, title);
+const done = function(err,self,fn) {
+    self.clearTimeout();
+    self.duration = new Date() - start;
+    finished = true;
+    if (!err && self.duration > ms && self._enableTimeouts) {
+        err = new Error('Timeout of ' + ms +
+            'ms exceeded. For async tests and hooks, ensure "done()" is called; if returning a Promise, ensure it resolves.');
+    }
+    fn(err);
+}
+export const test = (desc, fn) => spush('tests', [desc, fn]);
+export const testSuite = (title, testfn) => {
+  if (isEmptyStack()) {
+    execTestSuite(title, testfn);
+    return;
+  }
+
+  spush('testSuites', [title, testfn]);
+};
+export const setup = spush.bind(null, 'setup');
+export const teardown = spush.bind(null, 'teardown');
+export const testDone = (err,self,fn) => done(err,self,fn)
